3fdb9983f54fb84afeee934b51cb38141c671c63
vaibhav <codweird5@gmail.com>
2025-09-13 20:51:01 +0530

    added server file

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(morgan("dev"));
 
 app.use("/api/logs", logRoutes);
