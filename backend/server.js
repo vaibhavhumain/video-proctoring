e14e4f6dc21806210b1bef7a14fefa9b007241f1
Akash Dhiman <akashdhiman96252@gmail.com>
2025-09-15 15:53:20 +0530

    last mayybe

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["https://videoproctoringapp.netlify.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
