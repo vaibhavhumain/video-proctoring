e75d0a6bdd45c92fd321ca0be09ce31fa2638e4b
Akash Dhiman <akashdhiman96252@gmail.com>
2025-09-15 15:57:49 +0530

    skls

--- yolov8-export/app.py
+++ yolov8-export/app.py
@@ -9,10 +9,10 @@ import requests
 
 app = FastAPI()
 
-origins = ["http://localhost:5173", "http://127.0.0.1:5173"]
+origins = ["http://localhost:5173", "http://127.0.0.1:5173" , "https://videoproctoringapp.netlify.app"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
