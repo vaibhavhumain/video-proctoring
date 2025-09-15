b8a23c74337ab8aebe80b218e600ee42191a2671
vaibhav <codweird5@gmail.com>
2025-09-15 09:17:13 +0530

    Aml;d

--- backend/routes/logRoutes.js
+++ backend/routes/logRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createLog, getLogsByCandidate } from "../controllers/logController.js";
+import { createLog, getLogsByCandidate, getViolationsReport } from "../controllers/logController.js";
 
 const router = express.Router();
 
 router.post("/", createLog); 
 router.get("/:candidateId", getLogsByCandidate); 
-
+router.get("/violations", getViolationsReport);
 export default router;
